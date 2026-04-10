30c3a8e8405395f4d116dd4cb0d36e322fce9342
Josef Rittiner <joe.rittiner@gmail.com>
2026-04-10 10:10:54 +0200

    doc: Initialize Sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TextEditor'
+copyright = '2026, Joe Rittiner'
+author = 'Joe Rittiner'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../../tests'))
+
+extensions = [
+    'sphinx.ext.napoleon',  # Allows NumPy style docstrings
+]
+
+templates_path = ['_templates']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
