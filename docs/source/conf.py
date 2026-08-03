d28f145180d8470af5dd995940303d968f25332f
Josef Rittiner <joe.rittiner@gmail.com>
2026-08-04 00:07:38 +0200

    doc: Change docs Theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 
 needs_id_regex = r"^[A-Z0-9-]{3,}"
